9c5ae34ce629358eb9b918709bf4a7683526da97
LucioFider <fidelislucio404@gmail.com>
2023-01-06 13:27:16 +1000

    2nd-commit

--- src/App.tsx
+++ src/App.tsx
@@ -45,35 +45,102 @@ const slides = [
   },
 ];
 
+const slideWidth = 400;
+const slideMargin = 20;
+
+const scrollToSlide = (slider: HTMLUListElement | null, slideIndex: number) => {
+  if (!slider) return;
+  slider.scrollTo({
+    left: slideIndex * (slideWidth + slideMargin),
+    behavior: "smooth",
+  });
+};
+
 function App() {
+  const sliderRef = useRef<HTMLUListElement | null>(null);
+  const [sliderPosition, setSliderPosition] = useState(0);
+
+  const currentSlide = useMemo(() => {
+    return Math.floor(sliderPosition / (slideWidth + slideMargin));
+  }, [sliderPosition]);
+
+  const scrolledToEndOfSlider = useMemo(() => {
+    if (!sliderRef.current) return false;
+    return (
+      sliderRef.current.scrollWidth -
+        sliderRef.current.scrollLeft -
+        sliderRef.current.clientWidth ===
+      0
+    );
+  }, [sliderPosition]);
+
+  const goToNextSlide = useCallback(() => {
+    scrollToSlide(sliderRef.current, currentSlide + 1);
+  }, [currentSlide]);
+
+  const goToPreviousSlide = useCallback(() => {
+    scrollToSlide(sliderRef.current, currentSlide - 1);
+  }, [currentSlide]);
+
   return (
     <div className="bg-gray-200 min-h-screen font-sans py-16">
-      <h2 className="font-semibold text-6xl text-center max-w-[800px] mx-auto">
-        A magical connection to your devices
+      <h2 className="font-semibold text-6xl text-center max-w-[800px] mx-auto mb-20">
+        A magical connection to your devices.
       </h2>
-      <ul className="flex h-[500px] overflow-x-auto">
-        {slides.map((slide) => (
-          <li
-            className="bg-white flex flex-col w-[400px] rounded-xl shrink-0 mr-5 last:mr-0"
-            key={slide.title}
-          >
-            <div
-              className={classNames(
-                "flex h-full justify-center",
-                slide.centered ? "items-center" : "items-start"
-              )}
+
+      <div className="h-[500px] overflow-hidden">
+        <ul
+          ref={sliderRef}
+          onScroll={(ev) => {
+            setSliderPosition(ev.currentTarget.scrollLeft);
+          }}
+          className="flex h-[540px] pb-10 overflow-x-auto snap-x snap-mandatory"
+        >
+          {slides.map((slide) => (
+            <li
+              className="snap-start snap-always shrink-0 mr-5 last:mr-0"
+              key={slide.title}
             >
-              <img
-                src={slide.img}
-                alt={slide.title}
-                width={slide.imageWidth}
-                height={slide.imageHeight}
-              />
-            </div>
-            <h3 className="mt-auto">{slide.title}</h3>
-          </li>
-        ))}
-      </ul>
+              <div className="slide-center relative flex h-full flex-col bg-white w-[400px] rounded-2xl">
+                <div
+                  className={classNames(
+                    "flex h-full justify-center",
+                    slide.centered ? "items-center" : "items-start"
+                  )}
+                >
+                  <img
+                    src={slide.img}
+                    alt={slide.title}
+                    width={slide.imageWidth}
+                    height={slide.imageHeight}
+                  />
+                </div>
+                <h3 className="mt-auto text-2xl font-semibold p-6">
+                  {slide.title}
+                </h3>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex justify-center mt-4">
+        <button
+          disabled={currentSlide === 0}
+          onClick={() => goToPreviousSlide()}
+          className="disabled:text-gray-400 disabled:border-gray-400 w-8 h-8 border-2 border-black rounded-full flex items-center justify-center mr-2"
+        >
+          <span className="sr-only">Previous slide</span>
+          <Chevron className="w-3 h-3" />
+        </button>
+        <button
+          disabled={scrolledToEndOfSlider || currentSlide === slides.length}
+          onClick={() => goToNextSlide()}
+          className="disabled:text-gray-400 disabled:border-gray-400  w-8 h-8 border-2 border-black rounded-full flex items-center justify-center"
+        >
+          <span className="sr-only">Next slide</span>
+          <Chevron className="rotate-180 w-3 h-3" />
+        </button>
+      </div>
     </div>
   );
 }
